759f147657347a55671a72321f838a598b57c33a
selinasuzeroglu <selina.suzeroglu@gmail.com>
2022-12-01 12:25:49 +1100

    adding files

---
+++ Data_Evaluation/try1.py
@@ -0,0 +1,34 @@
+import serial
+import time
+from SQL_in_Python import spectrum
+
+
+def Microswitch():
+
+    MicroSwitch = serial.Serial('COM6', 9600, timeout=1)
+    InProcess = serial.Serial('COM9', 9600, timeout=1)
+    time.sleep(2)
+    MicroSwitch.flushInput()
+    InProcess.flushInput()
+
+    while True:
+        try:
+            Micro_bytes = MicroSwitch.readline()
+            decoded_bytes = float(Micro_bytes[0:len(Micro_bytes)-2].decode("utf-8"))
+            if decoded_bytes == float(0.0):
+                InProcess.write((bytes([0])))
+                print(spectrum)
+                break
+            else:
+                print("Sample Holder NOT in Position")
+
+        except:
+            print("Interrupt")
+            break
+
+
+    MicroSwitch.close()
+    InProcess.close()
+
+
+Microswitch()
