b078dce606c90388c155633938bb26eb68ae0b52
selinasuzeroglu <selina.suzeroglu@gmail.com>
2022-12-13 14:26:09 +1100

    Commit

--- Data_Evaluation/Plot_SQL_Data.py
+++ Data_Evaluation/Plot_SQL_Data.py
@@ -3,11 +3,22 @@ import numpy as np
 from SQL import *
 import pandas as pd
 
+
+class Plot:
+    def __init__(self, x_axis, y_axis):
+        self.x_axis = x_axis
+        self.y_axis = y_axis
+
+    def spectrum(self, x_axis_name, y_axis_name, title):
+        plt.plot(self.x_axis, self.y_axis)
+        plt.axis('tight')
+        plt.xlabel(x_axis_name)
+        plt.xlabel(y_axis_name)
+        plt.title(title)
+        plt.show()
+
+
 value = InProcessData('UVVis', 'Absorptance', values).get_specific_data(8)
 wavelength = InProcessData('UVVis', 'Absorptance', wavelengths).get_specific_data(8)
 
-
-plt.plot(wavelength, value)
-plt.axis('tight')
-plt.show()
-
+spectrum = Plot(wavelength, value).spectrum('wavelength', 'Absorptance', 'UVVis')
